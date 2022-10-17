c80c08ed7fce6fd9945e6b19374bc8a4e6acb4e1
Osamu Miyawaki <miyawaki@tungsten.cgd.ucar.edu>
2022-10-17 16:03:28 -0600

    distribution data now saves in its own variable subdirectory

--- hist_hotdays/era5/data/distribution/dist_t2m.py
+++ hist_hotdays/era5/data/distribution/dist_t2m.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 # this script creates a histogram of daily temperature for a given year
 # at each gridir point. 
 
+varn='t2m'
 lse = ['ann','djf','mam','jja','son'] # season (ann, djf, mam, jja, son)
 
 y0=1950 # first year
@@ -19,7 +20,7 @@ pc = [1e-3,1,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,82,85,87,90,92,95,97
 
 for se in lse:
     idir = '/project/mojave/observations/ERA5_daily/T2m'
-    odir = '/project/amp/miyawaki/data/p004/hist_hotdays/era5/%s' % (se)
+    odir = '/project/amp/miyawaki/data/p004/hist_hotdays/era5/%s/%s' % (se,varn)
 
     if not os.path.exists(odir):
         os.makedirs(odir)
@@ -43,4 +44,4 @@ for se in lse:
                 lt = t2m[:,la,ln]
                 ht2m[:,la,ln]=np.percentile(lt,pc)	
 
-        pickle.dump([ht2m, gr], open('%s/ht2m_%s.%s.pickle' % (odir,yr,se), 'wb'), protocol=5)	
+        pickle.dump([ht2m, gr], open('%s/h%s_%s.%s.pickle' % (odir,varn,yr,se), 'wb'), protocol=5)	
