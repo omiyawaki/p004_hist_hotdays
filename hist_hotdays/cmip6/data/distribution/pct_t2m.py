3c96ff44af1d2a6aea5962e85c7fccab9d24f369
Osamu Miyawaki <miyawaki@tungsten.cgd.ucar.edu>
2022-11-08 09:33:13 -0700

    20221108

--- hist_hotdays/cmip6/data/distribution/pct_t2m.py
+++ hist_hotdays/cmip6/data/distribution/pct_t2m.py
@@ -16,61 +16,62 @@ realm='atmos' # data realm e.g., atmos, ocean, seaIce, etc
 freq='day' # data frequency e.g., day, mon
 varn='tas' # variable name
 
-lfo = ['ssp245'] # data to use from 2015 onward (e.g., ssp245)
+fo='historical'
 lse = ['jja'] # season (ann, djf, mam, jja, son)
 # lse = ['jja','mam','son','djf'] # season (ann, djf, mam, jja, son)
 lcl = ['tseries']
-byr=[1950,2020] # output year bounds
+byr=[1950,2015] # output year bounds
 lyr=np.arange(byr[0],byr[1]+1)
 
 # percentiles to compute (follows Byrne [2021])
 pc = [1e-3,1,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,82,85,87,90,92,95,97,99] 
 
-for se in lse:
-    for fo in lfo:
+for cl in lcl:
+    for se in lse:
         # list of models
         lmd=mods(fo)
 
         # years and simulation names
         for imd in tqdm(range(len(lmd))):
             md=lmd[imd]
-            sim=simu(fo,cl)
+            ens=emem(md)
             grd=grid(fo,cl,md)
 
-            idir='/project/cmip6/%s/%s/%s/%s/%s/%s' % (sim,freq,varn,md,ens,grd)
+            idir='/project/amp/miyawaki/temp/cmip6/%s/%s/%s/%s/%s/%s' % (fo,freq,varn,md,ens,grd)
             odir='/project/amp/miyawaki/data/p004/hist_hotdays/cmip6/%s/%s/%s/%s' % (se,cl,fo,md)
 
             if not os.path.exists(odir):
                 os.makedirs(odir)
 
             c=0 # counter
-            fn = '%s/%s_%s_%s_%s_%s_%s_*.nc' % (idir,varn,freq,md,sim,ens,grd)
+            fn = '%s/%s_%s_%s_%s_%s_%s_*.nc' % (idir,varn,freq,md,fo,ens,grd)
 
             ds = xr.open_mfdataset(fn)
             t2m = ds[varn]
 
-            # select data within time of interest
-            t2m=t2m.sel(time=t2m['time.year']>=byr[0])
-            t2m=t2m.sel(time=t2m['time.year']<=byr[1])
+            for yr in tqdm(lyr):
+                # select data within time of interest
+                t2ms=t2m.sel(time=t2m['time.year']>=byr[0])
+                t2ms=t2m.sel(time=t2m['time.year']<=byr[1])
 
-            # select seasonal data if applicable
-            if se != 'ann':
-                t2m=t2m.sel(time=t2m['time.season']==se.upper())
-                        
-            # save grid info
-            gr = {}
-            gr['lon'] = ds['lon']
-            gr['lat'] = ds['lat']
-            ds.close()
-            t2m.load()
+                # select seasonal data if applicable
+                if se != 'ann':
+                    t2ms=t2ms.sel(time=t2ms['time.season']==se.upper())
+                            
+                # save grid info
+                gr = {}
+                gr['lon'] = ds['lon']
+                gr['lat'] = ds['lat']
+                ds.close()
+                t2ms.load()
 
-            # initialize array to store histogram data
-            ht2m = np.empty([len(pc), gr['lat'].size, gr['lon'].size])
+                # initialize array to store histogram data
+                ht2ms = np.empty([len(pc), gr['lat'].size, gr['lon'].size])
 
-            # loop through gridir points to compute percentiles
-            for ln in tqdm(range(gr['lon'].size)):
-                for la in range(gr['lat'].size):
-                    lt = t2m[:,la,ln]
-                    ht2m[:,la,ln]=np.percentile(lt,pc)	
+                # loop through gridir points to compute percentiles
+                for ln in tqdm(range(gr['lon'].size)):
+                    for la in range(gr['lat'].size):
+                        lt = t2ms[:,la,ln]
+                        ht2ms[:,la,ln]=np.percentile(lt,pc)	
 
-            pickle.dump([ht2m, gr], open('%s/ht2m_%g-%g.%s.pickle' % (odir,byr[0],byr[1],se), 'wb'), protocol=5)	
+                pickle.dump([ht2ms, gr], open('%s/ht2ms_%g-%g.%s.pickle' % (odir,byr[0],byr[1],se), 'wb'), protocol=5)	
